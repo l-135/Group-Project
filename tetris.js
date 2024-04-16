4937e995e5408b41acf1b0429c2eb3030e9476d5
Luis Gomez <luis.gomez.9267@gmail.com>
2024-04-16 13:06:46 -0700

    fixed typo

--- tetris.js
+++ tetris.js
@@ -7,7 +7,7 @@ function createGrid(board){
     for(let j = 0; j < 10; j++){
       const cell = document.createElement('div');
       cell.classList.add('cell');
-      board.appendchild('cell')
+      board.appendChild('cell')
     }
   }
 }
