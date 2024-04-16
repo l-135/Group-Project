7b79763479b4f41ccbc0ccca449c92cfd248b79b
Luis Gomez <luis.gomez.9267@gmail.com>
2024-04-16 13:43:45 -0700

    fixed typo

--- tetris.js
+++ tetris.js
@@ -2,12 +2,12 @@ const board1=document.getElementById('board1');
 const board2=document.getElementById('board2');
 
 //creates grid for both players
-function createGrid(board){
+function createGrid(surface){
   for(let i=0; i < 20; i++){
     for(let j = 0; j < 10; j++){
       const cell = document.createElement('div');
-      cell.classList.add('cell');
-      board.appendChild(cell);
+      cell.classList.add('grid');
+      surface.appendChild(cell);
     }
   }
 }
