9ea9747fd5664da3c5d3f6d5e03c4b95fe54e9cc
Luis Gomez <luis.gomez.9267@gmail.com>
2024-04-18 21:11:36 -0700

    added row and col #s

--- tetris.js
+++ tetris.js
@@ -1,5 +1,6 @@
 const board1=document.getElementById('board1');
 const board2=document.getElementById('board2');
+let gameBoard = [[],[]];
 
 //creates grid for both players
 function createGrid(surface){
@@ -7,9 +8,11 @@ function createGrid(surface){
     for(let j = 0; j < 10; j++){
       const cell = document.createElement('div');
       cell.classList.add('grid');
+      cell.id = i + '-' + j;
       surface.appendChild(cell);
     }
   }
 }
+
 createGrid(board1);
 createGrid(board2);
