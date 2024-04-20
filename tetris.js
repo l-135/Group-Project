c04bf5f4ff3aec7f68c0fe98f5903e386a30af23
Luis Gomez <luis.gomez.9267@gmail.com>
2024-04-19 22:16:52 -0700

    initialize 2d array

--- tetris.js
+++ tetris.js
@@ -1,11 +1,14 @@
+const row = 20;
+const col = 10; 
+const tetrisArray1 = [[],[]];
+const tetrisArray2 = [[],[]];
 const board1=document.getElementById('board1');
 const board2=document.getElementById('board2');
-let gameBoard = [[],[]];
 
 //creates grid for both players
 function createGrid(surface){
-  for(let i=0; i < 20; i++){
-    for(let j = 0; j < 10; j++){
+  for(let i=0; i < row; i++){
+    for(let j = 0; j < col; j++){
       const cell = document.createElement('div');
       cell.classList.add('grid');
       cell.id = i + '-' + j;
@@ -14,5 +17,17 @@ function createGrid(surface){
   }
 }
 
+//creates 2d array 
+function setArray(boardArray){
+  for(let i=0; i < row; i++){
+    for(let j = 0; j < col; j++){
+      //false = empty cell
+      boardArray[i][j] = false;
+    }
+  }
+}
+
 createGrid(board1);
 createGrid(board2);
+setArray(tetrisArray1);
+setArray(tetrisArray2);
